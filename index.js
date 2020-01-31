09bad44a81805312a97c799a5578138f7781db75
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-30 20:02:28 -0700

    Added SVG element to animate and appropriate supporting structure

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let eline = document.querySelector("#eline1 path")
+
+window.addEventListener("load", function () {
+   animationInit()
+})
+
+
+
+function animationInit() {
+   let fadeInTL = gsap.timeline()
+}
+GSDevTools.create();
