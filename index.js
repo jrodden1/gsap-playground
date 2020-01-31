8c30fd5cd01e55697df972ff88c4ad8130538dbb
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-30 20:13:39 -0700

    fixed index.js script load typo; basic animation fade-in of chart scales complete

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
-let eline = document.querySelector("#eline1 path")
+const efficiency = document.querySelector("#efficiency-scale")
+const time = document.querySelector("#time-scale")
+let fadeInTl = gsap.timeline()
+// I'll use you later
+// const eline = document.querySelector("#eline1 path")
 
 window.addEventListener("load", function () {
    animationInit()
@@ -7,7 +10,9 @@ window.addEventListener("load", function () {
 
 
 
+
 function animationInit() {
-   let fadeInTL = gsap.timeline()
+   fadeInTl.from([efficiency, time], {duration: 3, autoAlpha: 0, stagger: 0.5}, "+=2")
+   
 }
 GSDevTools.create();
